d41323dfbc33d7c59e529d4dfe19c5a61d04c16f
ashishu007 <panditu2015@gmail.com>
2022-07-11 11:48:21 +0100

    down sampled data

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from utils import DataLoader, Classifier, CalcPerformance
 from bert_utils import BertDataLoader, BertThemeClassifier
 from transformers import AutoModelForSequenceClassification
 
+def save_to_disk(train_x, train_y, val_x, val_y, test_x, test_y, THEME, DOWN=False):
+    print('Saving data to disk into csv...')
+    train = pd.DataFrame({"text": train_x, "label": list(train_y)})
+    val = pd.DataFrame({"text": val_x, "label": list(val_y)})
+    test = pd.DataFrame({"text": test_x, "label": list(test_y)})
+    train.to_csv(f'data/{THEME}/train_text{"_down" if DOWN else ""}.csv', index=False, header=False)
+    val.to_csv(f'data/{THEME}/validation_text{"_down" if DOWN else ""}.csv', index=False, header=False)
+    test.to_csv(f'data/{THEME}/test_text{"_down" if DOWN else ""}.csv', index=False, header=False)
+
 
 def main(FTR='text', CLF='rf', THEME='streak', DOWNSAMPLE=False, SAVE_TO_DISK=False):
 
@@ -23,13 +32,6 @@ def main(FTR='text', CLF='rf', THEME='streak', DOWNSAMPLE=False, SAVE_TO_DISK=Fa
     print('Processing data...')
     all_data = theme_obj.get_theme_train_val_test_data()
 
-    if SAVE_TO_DISK:
-        print('Saving data to disk...')
-        for part in ['train', 'validation', 'test']:
-            df = pd.DataFrame(all_data[part])
-            df.to_csv(f'data/{THEME}/{part}_{FTR}.csv', index=False)
-        return 
-
     if CLF == 'bert':
         clf_obj = BertThemeClassifier()
         dl_obj = BertDataLoader(downsample=DOWNSAMPLE)
@@ -38,6 +40,10 @@ def main(FTR='text', CLF='rf', THEME='streak', DOWNSAMPLE=False, SAVE_TO_DISK=Fa
         dataset, train_x, train_y, val_x, val_y, test_x, test_y = dl_obj.prep_data(all_data)
         print(f"Train: {len(train_x)} {train_y.shape}\tVal: {len(val_x)} {val_y.shape}\tTest: {len(test_x)} {test_y.shape}")
         print(Counter(train_y), Counter(val_y), Counter(test_y))
+
+        if SAVE_TO_DISK:
+            save_to_disk(train_x, train_y, val_x, val_y, test_x, test_y, THEME, DOWN=DOWNSAMPLE)
+            return 
 
         trained_model, tokenizer = clf_obj.train(dataset)
 
@@ -57,6 +63,11 @@ def main(FTR='text', CLF='rf', THEME='streak', DOWNSAMPLE=False, SAVE_TO_DISK=Fa
 
         train_x, train_y, val_x, val_y, test_x, test_y = dl_obj.prep_data(all_data)
         print(f"Train: {train_x.shape} {train_y.shape}\tVal: {val_x.shape} {val_y.shape}\tTest: {test_x.shape} {test_y.shape}")
+        print(Counter(train_y), Counter(val_y), Counter(test_y))
+
+        if SAVE_TO_DISK:
+            save_to_disk(train_x, train_y, val_x, val_y, test_x, test_y, THEME, DOWN=DOWNSAMPLE)
+            return 
 
         print('Training classifier...')
         model = clf_obj.train(train_x, train_y)
